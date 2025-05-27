2b9e8d51615957463c7ce48db0d540a770d74b0b
LordAfterEight <elias@stettmayer.de>
2025-05-27 14:28:02 +0200

    Started work on UI

--- src/app.rs
+++ src/app.rs
@@ -2,44 +2,100 @@ use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
     buffer::Buffer,
     layout::Rect,
-    style::Stylize,
+    style::{Stylize, Color},
     symbols::border,
     text::{Line, Text},
-    widgets::{Block, Paragraph, Widget},
+    widgets::{Block, Paragraph, Widget, Padding},
+    prelude::{Constraint, Layout},
     DefaultTerminal, Frame,
 };
 use std::io;
 
 #[derive(Debug, Default)]
 pub struct App {
-    exit: bool
+    exit: bool,
+    bg_color: u8,
+    fg_color: u8,
+    sidebar_bg: u8,
+    sidebar_fg: u8
 }
 
 impl App {
-    pub fn run(&mut self, terminal: &mut DefaultTerminal)
-        -> io::Result<()> {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) {
+
+        self.bg_color = 233;
+        self.fg_color = 147;
+
+        self.sidebar_bg = 234;
+        self.sidebar_fg = 147;
 
         while !self.exit {
-            terminal.draw(|frame| self.draw(frame))?;
-            self.handle_events()?;
+            terminal.draw(|frame| {
+
+                let title = Line::from(" File Manager ".bold());
+                let sidebar_title = Line::from(" Files ".bold());
+
+
+                let instructions = Line::from(vec![
+                    " Quit ".into(),
+                    "<Q> ".light_blue().bold(),
+                ]);
+
+                let block_bg = Block::bordered()
+                    .title_bottom(instructions.centered())
+                    .border_set(border::ROUNDED)
+                    .padding(Padding::proportional(1))
+                    .bg(Color::Indexed(self.bg_color))
+                    .fg(Color::Indexed(self.fg_color));
+
+                let block_sidebar = Block::bordered()
+                    .border_set(border::ROUNDED)
+                    .padding(Padding::proportional(1))
+                    .bg(Color::Indexed(self.sidebar_bg))
+                    .fg(Color::Indexed(self.sidebar_fg));
+
+
+                let block = Paragraph::new("Hello World".bold())
+                    .centered()
+                    .block(Block::bordered()
+                        .border_set(border::ROUNDED)
+                        .padding(Padding::proportional(2))
+                        .bg(Color::Indexed(self.bg_color))
+                        .fg(Color::Indexed(self.fg_color))
+                        .title(" Block "));
+
+                let sidebar = Paragraph::new("Files")
+                    .centered()
+                    .block(Block::bordered()
+                        .border_set(border::ROUNDED)
+                        .padding(Padding::proportional(2))
+                        .bg(Color::Indexed(self.sidebar_bg))
+                        .fg(Color::Indexed(self.sidebar_fg))
+                        .title(" Files "));
+
+
+                let [left, right] = Layout::horizontal([Constraint::Percentage(50); 2])
+                    .areas(frame.area());
+                let [top, middle, bottom] = Layout::vertical([Constraint::Fill(1); 3])
+                    .areas(right);
+
+                frame.render_widget(block, right);
+                frame.render_widget(sidebar, left);
+
+            });
+            self.handle_events()
         }
-        Ok(())
     }
 
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
-    }
-
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
+    fn handle_events(&mut self){
+        match event::read() {
             // it's important to check that the event is a key press event as
             // crossterm also emits key release and repeat events on Windows.
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+            Ok(Event::Key(key_event)) if key_event.kind == KeyEventKind::Press => {
                 self.handle_key_event(key_event)
             }
             _ => {}
         };
-        Ok(())
     }
 
     fn handle_key_event(&mut self, key_event: KeyEvent) {
@@ -51,30 +107,5 @@ impl App {
 
     fn exit(&mut self) {
         self.exit = true;
-    }
-}
-
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from(" Counter App Tutorial ".bold());
-        let instructions = Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
-        let block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
-            .border_set(border::THICK)
-            .on_dark_gray();
-
-
-        Paragraph::new("Hello World".bold())
-            .centered()
-            .block(block)
-            .render(area, buf);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ mod app;
 
 fn main() {
     let mut terminal = ratatui::init();
-    let app_result = app::App::default().run(&mut terminal);
+
+    let app_result = app::App::default()
+        .run(&mut terminal);
     ratatui::restore();
-    app_result.unwrap()
+    app_result
 }
